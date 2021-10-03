8a856c14b0f9774f656284977dbe2a963ddf2f31
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-10-03 20:36:26 -0300

    Encounter 1° Commit

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RoleplayGame;
+using System.Collections;
 
 namespace Program
 {
@@ -7,14 +8,14 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            SpellsBook book = new SpellsBook();
+           /* SpellsBook book = new SpellsBook();
             book.AddSpell(new SpellOne());
             book.AddSpell(new SpellOne());
 
             Wizard gandalf = new Wizard("Gandalf");
             gandalf.AddMagicItem(book);/*cambio a AddMagicItem proque distingui en la classe IMagicalcharacter 
             el agregar o remover iteams magicos o normales */
-
+            /*
             Dwarf gimli = new Dwarf("Gimli");
 
             Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
@@ -27,6 +28,26 @@ namespace Program
             gimli.Cure();
 
             Console.WriteLine($"Someone cured Gimli. Gimli now has ❤️ {gimli.Health}");
+            */
+            Knight Green=new Knight("Green");
+            Dwarf  Brown=new Dwarf("Brown");
+            Archer Blue=new Archer("Blue");
+            Cyclops Cyclops01= new Cyclops("Cyclops01");
+            Cyclops Cyclops02= new Cyclops("Cyclops02");
+            Cyclops Cyclops03= new Cyclops("Cyclops03");
+            ArrayList Team_H= new ArrayList();
+            ArrayList Team_E= new ArrayList();
+            Team_H.Add(Green);
+            Team_H.Add(Blue);
+            Team_H.Add(Brown);
+            Team_E.Add(Cyclops01);
+            Team_E.Add(Cyclops02);
+            Team_E.Add(Cyclops03);
+            
+
+
+
+
         }
     }
 }
